8ecbb88d651d013227b5025317eb1a90289f46e5
Lil JSF <90755554+KRBNJSF@users.noreply.github.com>
2022-12-27 23:34:21 +0100

    Update client.js

--- client/client.js
+++ client/client.js
@@ -1,7 +1,9 @@
 const socket = io("http://localhost:3000");
 
+//Sending message to the server
 socket.emit("clientWelcome", 5, "jojo");
 
+//Receiving message from the server
 socket.on("serverWelcome", (...args) => {
   console.log(args);
 });
